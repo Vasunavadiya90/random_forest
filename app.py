c9ac6da42aef48970fa2d2cc943531b9bfb9819f
Vasunavadiya90 <vasunavadiya923@gmail.com>
2025-09-17 17:54:54 +0530

    changesx

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 def sum(a,b):
     return a+b
 
+
 def recurssion(n):
     if n==1 or n==0:
         return 1
     else:
         return n*recurssion(n-1)
     
+    
 def factorial(n):
     if n==0 or n==1:
         return 1
     else:
         return n*factorial(n-1)
+
+def division(a,b):
+    return a/b
